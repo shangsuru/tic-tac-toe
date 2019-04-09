ca60eef7832736c9251ee687ee1113cd14c663e9
shangsuru <helm.henry99@gmail.com>
2019-04-09 07:09:57 +0200

    refactoring

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -1,6 +1,5 @@
 let fields = document.querySelectorAll('td')
 let numberOfTurns = 0
-let gameTerminated = false
 
 /* 
  * Places a mark on the field the player clicked on, if it's empty
@@ -13,9 +12,7 @@ fields.forEach(function (elem) {
       numberOfTurns++
       this.innerHTML = '<i class="fas fa-times fa-5x"></i>'
     }
-
-    checkWinCondition()
-    if (gameTerminated || numberOfTurns === 9) {
+    if (isGameFinished()) {
       setTimeout(newGame, 1000)
     } else {
       setTimeout(computerMove, 500)
@@ -23,79 +20,91 @@ fields.forEach(function (elem) {
   })
 })
 
-// let the computer make its move and check win condition afterwards
+// Let the computer make its move and check if game terminated.
 function computerMove() {
   numberOfTurns++
 
-  // choose random field and put a mark on it
-  let n = Math.floor(Math.random() * 9)
-  while (fields[n].innerHTML !== '') {
-    n = Math.floor(Math.random() * 9)
-  }
-  fields[n].innerHTML = '<i class="far fa-circle fa-4x"></i>'
+  // choose random empty field and put a mark on it
+  let emptyFields = getEmptyFields()
+  let randomIndex = Math.floor(Math.random()* emptyFields.length)
+  emptyFields[randomIndex].innerHTML = '<i class="far fa-circle fa-4x"></i>'
 
-  checkWinCondition()
-  if (gameTerminated || numberOfTurns === 9) {
+  if (isGameFinished()) {
     setTimeout(newGame, 1000)
   }
 }
 
-/* Checks if there are three equal marks in a row column or diagonal
- * and if so, changes the background of
- * those to red and sets the gameTerminated flag to true.
+function getEmptyFields() {
+  let emptyFields =  []
+  for (let field of fields) {
+    if (field.innerHTML === '') {
+      emptyFields.push(field)
+    }
+  }
+  return emptyFields
+}
+
+/* Returns true if there is a mark on each field or if
+ * there are three equal marks in a row, column or diagonal.
  */
-function checkWinCondition() {
+function isGameFinished() {
   // rows
   if (fields[0].innerHTML !== '' && fields[0].innerHTML === fields[1].innerHTML && fields[1].innerHTML === fields[2].innerHTML) {
     fields[0].style.background = '#8b0000'
     fields[1].style.background = '#8b0000'
     fields[2].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   if (fields[3].innerHTML !== '' && fields[3].innerHTML === fields[4].innerHTML && fields[4].innerHTML === fields[5].innerHTML) {
     fields[3].style.background = '#8b0000'
     fields[4].style.background = '#8b0000'
     fields[5].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   if (fields[6].innerHTML !== '' && fields[6].innerHTML === fields[7].innerHTML && fields[7].innerHTML === fields[8].innerHTML) {
     fields[6].style.background = '#8b0000'
     fields[7].style.background = '#8b0000'
     fields[8].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   // columns
   if (fields[0].innerHTML !== '' && fields[0].innerHTML === fields[3].innerHTML && fields[3].innerHTML === fields[6].innerHTML) {
     fields[0].style.background = '#8b0000'
     fields[3].style.background = '#8b0000'
     fields[6].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   if (fields[1].innerHTML !== '' && fields[1].innerHTML === fields[4].innerHTML && fields[4].innerHTML === fields[7].innerHTML) {
     fields[1].style.background = '#8b0000'
     fields[4].style.background = '#8b0000'
     fields[7].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   if (fields[2].innerHTML !== '' && fields[2].innerHTML === fields[5].innerHTML && fields[5].innerHTML === fields[8].innerHTML) {
     fields[2].style.background = '#8b0000'
     fields[5].style.background = '#8b0000'
     fields[8].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   // diagonals
   if (fields[0].innerHTML !== '' && fields[0].innerHTML === fields[4].innerHTML && fields[4].innerHTML === fields[8].innerHTML) {
     fields[0].style.background = '#8b0000'
     fields[4].style.background = '#8b0000'
     fields[8].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
   if (fields[2].innerHTML !== '' && fields[2].innerHTML === fields[4].innerHTML && fields[4].innerHTML === fields[6].innerHTML) {
     fields[2].style.background = '#8b0000'
     fields[4].style.background = '#8b0000'
     fields[6].style.background = '#8b0000'
-    gameTerminated = true
+    return true
   }
+
+  if (numberOfTurns === 9) {
+    return true
+  }
+
+  return false
 }
 
 /*
@@ -104,9 +113,7 @@ function checkWinCondition() {
  * arbitrarily determines a starting player.
  */
 function newGame() {
-  gameTerminated = false
   numberOfTurns = 0
-  opponentsTurn = false
 
   fields.forEach(function (elem) { 
     elem.innerHTML = ''
